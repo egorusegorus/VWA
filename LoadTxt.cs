a3eff8114ce949638ffb08144778ad753c4675ac
egorusegorus <tomaszokraszewski@gmail.com>
2024-12-04 23:10:56 +0100

    MI One

---
+++ LoadTxt.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace VWA
+{
+	
+		class LoadTxt { 
+		public string ReadEmbeddedTextFile(string resourceName)
+		{
+			string content = string.Empty;
+
+			
+			using (Stream stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName))
+			{
+				if (stream != null)
+				{
+					using (StreamReader reader = new StreamReader(stream, Encoding.UTF8))
+					{
+						content = reader.ReadToEnd(); 
+					}
+				}
+				else
+				{
+					content = "Ressourcen nicht gefunden."; 
+				}
+			}
+
+			return content;
+		}
+		
+	}
+}
--- Login.xaml.cs
+++ Login.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -15,14 +17,27 @@ using System.Windows.Shapes;
 
 namespace VWA
 {
-    /// <summary>
-    /// Interaktionslogik für Login.xaml
-    /// </summary>
-    public partial class Login : UserControl
+	/// <summary>
+	/// Interaktionslogik für Login.xaml
+	/// </summary>
+	public partial class Login : UserControl
     {
         public Login()
         {
             InitializeComponent();
         }
-    }
-}
+
+		private void btnLogin_Click(object sender, RoutedEventArgs e)
+		{
+			string User = txtUser.Text;
+			string Password = txtPassword.Password;
+			LoadTxt loadTxt = new LoadTxt();
+			string fileContent = loadTxt.ReadEmbeddedTextFile("VWA.SConfig.txt");
+			fileContent=fileContent+";"+ "Uid ="+User+";"+"Pwd ="+Password;
+			bottomPanel.Content = fileContent;
+		}
+	}
+	
+	}
+
+
